2dcaa759fb513fadb7f5fe49b16de43710fb60c9
資海 <@EEIT>
2019-01-03 14:47:33 +0800

    基本DAO

--- roy/src/main/java/model/dao/BackerDAO.java
+++ roy/src/main/java/model/dao/BackerDAO.java
@@ -21,7 +21,7 @@ public class BackerDAO {
 //	return this.sessionFactory.getCurrentSession();
 //}
 	
-	
+	 
 	public static void main(String... args) throws IOException, Exception, SQLException {
 		SessionFactory sessionFactory = HibernateUtil.getSessionfactory();
 		Session session = sessionFactory.openSession();
