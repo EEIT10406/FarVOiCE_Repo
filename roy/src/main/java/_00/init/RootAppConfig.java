059679e8fcaca4d8a035c7b48da23102eb2b2856
資海 <@EEIT>
2019-01-23 11:29:12 +0800

    客服service dao bean

--- roy/src/main/java/_00/init/RootAppConfig.java
+++ roy/src/main/java/_00/init/RootAppConfig.java
@@ -15,8 +15,8 @@ import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import model.bean.ReportBean;
 import model.bean.BackerBean;
+import model.bean.CustomerServiceBean;
 import model.bean.FollowBean;
 import model.bean.FundingBean;
 import model.bean.ListMusicBean;
@@ -26,6 +26,7 @@ import model.bean.MemberLikeMusicBean;
 import model.bean.MusicBean;
 import model.bean.PlaylistBean;
 import model.bean.PostBean;
+import model.bean.ReportBean;
 import model.bean.RewardBean;
 import model.bean.StoryBean;
 
@@ -55,7 +56,7 @@ public class RootAppConfig {
 
 
 		builder.addAnnotatedClasses(MemberBean.class,PostBean.class,MusicBean.class,PlaylistBean.class,ListMusicBean.class,MemberCommentMusicBean.class,
-				FollowBean.class, BackerBean.class, FundingBean.class, RewardBean.class,StoryBean.class,MemberLikeMusicBean.class,ReportBean.class);
+				FollowBean.class, BackerBean.class, FundingBean.class, RewardBean.class,StoryBean.class,MemberLikeMusicBean.class,ReportBean.class,CustomerServiceBean.class);
 
 		Properties props = new Properties();
 		props.setProperty("hibernate.dialect", "org.hibernate.dialect.SQLServerDialect");
